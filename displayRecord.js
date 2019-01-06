a0dc03615723d2529ee8c0c622784726ec8e0cc1
RayFV <rayfvfree@outlook.com>
2019-01-06 21:08:45 +0800

    Updated Record

--- displayRecord.js
+++ displayRecord.js
@@ -3,16 +3,90 @@ const fs = require("fs");
 const moment = require("moment");
 const {remote} = require("electron");
 const {app} = remote;
+const {ipcRenderer} = require("electron");
 
 const appPath = app.getAppPath();
 //const appPath = process.env.PORTABLE_EXECUTABLE_DIR;
 
 const teamFilePath = appPath + '/teamFiles';
+const table = document.getElementById('recordTable');
+const tbody = document.createElement('tbody');
+table.appendChild(tbody);
 
 let allRecords = [];
+refreshFilesList();
 
 
-function readRecord(fileName){
+$('#record').on('click', () => {
+    ipcRenderer.send('RecordWindow');
+});
+
+$('#memorableDraw').on('click', () => {
+    ipcRenderer.send('memorableDrawWindow');
+});
+
+$('#normalDraw').on('click', () => {
+    ipcRenderer.send('normalDrawWindow');
+});
+
+$('body').on('click', '#files .file', function(event){
+    let fileName = event.target.innerText;
+    console.log(fileName);
+
+    refreshTable(fileName);
+    refreshFuture(fileName);
+});
+
+function showTable(allRecords){
+    $('#recordTable tbody').empty();
+    for(let i = 0; i < allRecords.length ; i++){
+        let tr = document.createElement('tr');
+        
+        let th = document.createElement('th');
+        th.setAttribute("scope", "row");
+        th.innerHTML = i + 1;
+        tr.appendChild(th);
+
+        let content_td = document.createElement('td');
+        content_td.innerHTML = allRecords[i].content;
+        console.log(allRecords[i].content);
+        tr.appendChild(content_td);
+
+        let drawTime_td = document.createElement('td');
+        drawTime_td.innerHTML = allRecords[i].drawTime;
+        tr.appendChild(drawTime_td);
+
+        tbody.appendChild(tr);
+    }
+}
+
+function showFuture(teams){
+    let totalWeight = 0;
+    for(let i = 0; i < teams.length; i++){
+        totalWeight += teams[i].weight;
+    }
+
+    let result = "";
+    for(let i = 0; i <teams.length; i++){
+        result = result + teams[i].name + "_" + Math.floor((teams[i].weight/totalWeight) * 100) + "_" + teams[i].count + "\n";
+    }
+    console.log(result);
+}
+
+function refreshFilesList(){
+    $('#files').empty();
+    fs.readdir(teamFilePath, function(err, files) {
+        console.log(files);
+        for (let i=0; i<files.length; i++) {
+            let newDiv = document.createElement('li');
+            newDiv.setAttribute("class", "file");
+            newDiv.innerHTML = files[i];
+            $('#files').append(newDiv);
+        }
+    });
+ }
+
+ function refreshTable(fileName){
     let path = teamFilePath + "/" + fileName;
     fs.readFile(path, function(err, content){
         if(err){
@@ -20,19 +94,39 @@ function readRecord(fileName){
             return;
         }
         json = JSON.parse(content);
-
-        allRecords.push(json.record);
+        let records = json.record;
         
+        let list = [];
+        for(let i = 0; i < records.length;i++){
+            list.push(records[i]);
+        }
+
+        showTable(list);
     });
  }
 
-function show(){
-    let table = document.getElementById('');
-    let newDiv = document.createElement('li');
-            newDiv.setAttribute("class", "file");
-            newDiv.innerHTML = files[i];
-            $('#files').append(newDiv);
-}
+
+ function refreshFuture(fileName){
+    let path = teamFilePath + "/" + fileName;
+    fs.readFile(path, function(err, content){
+        if(err){
+            console.log(err);
+            return;
+        }
+        json = JSON.parse(content);
+        let records = json.teams;
+        
+        let list = [];
+        for(let i = 0; i < records.length;i++){
+            list.push(records[i]);
+        }
+
+        showFuture(list);
+    });
+ }
+
+
+
 
 function Team(id, name, weight, count){
     this.id = id;
